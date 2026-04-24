f7f2366ad86ef4537a318e0494e2dbfdb1ec203b
Bruno Zorić <bruno.zoric@gmail.com>
2026-04-24 07:36:55 +0200

    wip: extensions

--- extensions/OnRecordModify/OnRecordModify.ts
+++ extensions/OnRecordModify/OnRecordModify.ts
@@ -3,7 +3,16 @@ import { RecordModifier } from "~/index.js";
 class OnRecordModifyImpl implements RecordModifier.Interface {
   public async modify(options: RecordModifier.ModifyOptions): Promise<Record<string, unknown>> {
     const { record, table, sourcePath } = options;
-    return record;
+
+    const index = record.index as string | undefined;
+    if (!index) {
+      return record;
+    }
+
+    return {
+      ...record,
+      index: index.replace("wby-z0044zvw-", "")
+    };
   }
 }
 
